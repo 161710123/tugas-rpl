665090659c90ff1ac18dba613ac1120e0ce91d7f
unknown <161710096@smkassalaambandung.sch.id>
2018-01-15 08:41:25 +0400

    dika

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+
+@extends('layouts.1')
+@section('konten')
+<!DOCTYPE html>
+<html>
+<head>
+	<title>tugas</title>
+</head>
+<body>
+<div class="jumbotron">
+        <marquee><h1>assalammualaikum</h1></marquee>
+        <p>"selamat datang di web saya".</p>
+      </div>
+</body>
+</html>
+@endsection
---
+++ resources/views/profil.blade.php
@@ -0,0 +1,19 @@
+
+@extends('layouts.1')
+@section('konten')
+<!DOCTYPE html>
+<html>
+<head>
+	<title>tugas</title>
+</head>
+<body bgcolor='red'>
+<h1>Nama : dika fadlan</h1>
+<h1>Umur : 16 tahun</h1>
+<h1>Kelas : XI Rpl 1</h1>
+<h1>Jurusan : RPL</h1>
+<h1>Hobi : Main Gitar</h1>
+<h1>Cita-cita :Programmer dan Gitaris</h1>
+
+</body>
+</html>
+@endsection
